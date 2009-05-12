f7c5ed04be13f1a086953b80b618e1c0fcef52d8
jkobrin <jkobrin@limelabs.com>
2009-05-12 16:42:54 -0400

    don't use RANGER_DIR variable

--- config.mk
+++ config.mk
@@ -2,9 +2,9 @@
 # .lime-config files found and read by config_engine.py. This environment will
 # be inhereited by subprocesses
 
-RANGER_DIR ?= .
+ENV_ENGINE_DIR ?= .
 CONFIG_FILE ?= .lime-config-generated.mk
 
-$(shell python $(RANGER_DIR)/config_engine.py make > $(CONFIG_FILE) )
+$(shell python $(ENV_ENGINE_DIR)/config_engine.py make > $(CONFIG_FILE) )
 
 include $(CONFIG_FILE)
